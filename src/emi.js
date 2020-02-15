e9f37fbfc10aef29ebdea9f3cb04e21377fc926c
rutvikmer <rutvikmer56@gmail.com>
2020-02-15 11:52:38 +0530

    Add test to validate whether Loan() allows negative values

--- src/emi.js
+++ src/emi.js
@@ -9,9 +9,9 @@
  */
 function Loan (amount, installmentsNumber, interestRate) {
   /** Checking params */
-  if (!amount ||
-     !installmentsNumber ||
-     !interestRate) {
+  if (!amount || amount <= 0 ||
+    !installmentsNumber || installmentsNumber <= 0 ||
+    !interestRate || interestRate <= 0) {
     throw new Error(`wrong parameters: ${amount} ${installmentsNumber} ${interestRate}`)
   }
 
@@ -20,7 +20,7 @@ function Loan (amount, installmentsNumber, interestRate) {
   let principalSum = 0
   let sum = 0
 
-  for (let i = 0; i <= installmentsNumber; i++) {
+  for (let i = 0; i < installmentsNumber; i++) {
     const inst = getNextInstallment(
       amount, installmentsNumber, interestRate, principalSum, interestSum
     )
