cacb2f710f4cfb0b791082fbf97dbc4f24c18722
hardik-erda <hardik.erda27@gmail.com>
2023-02-11 11:07:50 +0530

    Added test case for negative interest

--- src/emi.js
+++ src/emi.js
@@ -9,9 +9,10 @@
  */
 function Loan (amount, installmentsNumber, interestRate) {
   /** Checking params */
-  if (!amount ||
-     !installmentsNumber ||
-     !interestRate) {
+  // function Loan
+  if (!amount || amount <= 0 ||
+  !installmentsNumber || installmentsNumber <= 0 ||
+  !interestRate || interestRate <= 0) {
     throw new Error(`wrong parameters: ${amount} ${installmentsNumber} ${interestRate}`)
   }
 
