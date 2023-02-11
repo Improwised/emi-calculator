4ffe5c5ebb11a1a50e343c18a0f3d6e5f792ec95
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2023-02-11 10:38:43 +0530

    Fixed extra emi issue

--- src/emi.js
+++ src/emi.js
@@ -7,6 +7,8 @@
  *
  * @return {object}
  */
+
+// changed
 function Loan (amount, installmentsNumber, interestRate) {
   /** Checking params */
   if (!amount ||
@@ -39,7 +41,7 @@ function Loan (amount, installmentsNumber, interestRate) {
   }
 
   return {
-    installments: installments,
+    installments,
     amount: rnd(amount),
     interestSum: rnd(interestSum),
     principalSum: rnd(principalSum),
@@ -68,9 +70,9 @@ const getNextInstallment = (
   const principal = installment - interest
 
   return {
-    principal: principal,
-    interest: interest,
-    installment: installment,
+    principal,
+    interest,
+    installment,
     remain: amount - principalSum - principal,
     interestSum: interestSum + interest
   }
@@ -88,8 +90,8 @@ function emiToHtmlTable (loan, params) {
   params.formatMoney = params.formatMoney || function (num) {
     return num.toFixed(2)
   }
-  var fm = params.formatMoney
-  var html = [
+  const fm = params.formatMoney
+  const html = [
     '<table class="table table-striped">' +
       '<thead>' +
         '<tr>' +
@@ -107,9 +109,9 @@ function emiToHtmlTable (loan, params) {
     '</table>'
   ]
 
-  for (var i = 0; i < loan.installments.length; i++) {
-    var inst = loan.installments[i]
-    var instHtml =
+  for (let i = 0; i < loan.installments.length; i++) {
+    const inst = loan.installments[i]
+    const instHtml =
           '<tr>' +
             '<td>' + (i + 1) + '</td>' +
             '<td>' + fm(inst.principal) + '</td>' +
@@ -152,8 +154,8 @@ if (typeof module === 'undefined') {
 } else {
   // node or browserfy
   module.exports = {
-    Loan: Loan,
-    emiToHtmlTable: emiToHtmlTable,
-    rnd: rnd
+    Loan,
+    emiToHtmlTable,
+    rnd
   }
 }
