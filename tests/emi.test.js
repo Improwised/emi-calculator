cf52d94119338888a6ede6f144194b81b5544aa8
Vivek Kandoliya <vivekkandoliya123@gmail.com>
2023-03-18 12:17:41 +0530

    Add test to validate whether Loan() allows negative values

--- tests/emi.test.js
+++ tests/emi.test.js
@@ -21,4 +21,4 @@ test('Should calculate correct EMI installments', () => {
 
 test('Should throw an error on negative interest rate', () => {
   expect(() => EMI.Loan(10000, -1, 10)).toThrowError('wrong parameters: 10000 -1 10')
-});
+})
