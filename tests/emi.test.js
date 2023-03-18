4a951d42f70b01abb137f29529e47d8b7b6f6378
Yash Lathiya <yashlathiya59ppsv@gmail.com>
2023-03-18 12:16:11 +0530

    Added test case for -ve intrest rate

--- tests/emi.test.js
+++ tests/emi.test.js
@@ -1,20 +1,26 @@
-const EMI = require('../src/emi.js')
+const EMI = require("../src/emi.js");
 
-test('Loan is defined', () => {
-  expect(EMI.Loan).toBeDefined()
-})
+test("Loan is defined", () => {
+  expect(EMI.Loan).toBeDefined();
+});
 
-test('Should calculate correct EMI installments', () => {
-  const loan = EMI.Loan(10000, 12, 10)
+test("Should calculate correct EMI installments", () => {
+  const loan = EMI.Loan(10000, 12, 10);
 
-  expect(loan).toBeDefined()
-  expect(loan.installments.length).toBe(12)
-  expect(loan.installments[0].installment).toBe(879.16)
-  expect(loan.installments[12 - 1].installment).toBe(879.16)
-  expect(loan.amount).toBe(10000)
-  expect(loan.principalSum).toBe(10000)
-  expect(loan.interestSum).toBe(549.89)
-  expect(loan.sum).toBe(10549.89)
+  expect(loan).toBeDefined();
+  expect(loan.installments.length).toBe(12);
+  expect(loan.installments[0].installment).toBe(879.16);
+  expect(loan.installments[12 - 1].installment).toBe(879.16);
+  expect(loan.amount).toBe(10000);
+  expect(loan.principalSum).toBe(10000);
+  expect(loan.interestSum).toBe(549.89);
+  expect(loan.sum).toBe(10549.89);
 
-  expect(EMI.rnd(loan.principalSum + loan.interestSum)).toBe(loan.sum)
-})
+  expect(EMI.rnd(loan.principalSum + loan.interestSum)).toBe(loan.sum);
+});
+
+test("Should throw an error on negative interest rate", () => {
+  expect(() => EMI.Loan(10000, -1, 10)).toThrowError(
+    "wrong parameters: 10000 -1 10"
+  );
+});
