12d68538819951dab618a8ce89050c3069632417
janvi kanani <kananijanu912@gmail.com>
2023-04-08 11:56:58 +0530

    Fixed Wrong Emi Issue

--- src/emi.js
+++ src/emi.js
@@ -39,7 +39,7 @@ function Loan (amount, installmentsNumber, interestRate) {
   }
 
   return {
-    installments: installments,
+    installments,
     amount: rnd(amount),
     interestSum: rnd(interestSum),
     principalSum: rnd(principalSum),
@@ -68,9 +68,9 @@ const getNextInstallment = (
   const principal = installment - interest
 
   return {
-    principal: principal,
-    interest: interest,
-    installment: installment,
+    principal,
+    interest,
+    installment,
     remain: amount - principalSum - principal,
     interestSum: interestSum + interest
   }
@@ -88,8 +88,8 @@ function emiToHtmlTable (loan, params) {
   params.formatMoney = params.formatMoney || function (num) {
     return num.toFixed(2)
   }
-  var fm = params.formatMoney
-  var html = [
+  const fm = params.formatMoney
+  const html = [
     '<table class="table table-striped">' +
       '<thead>' +
         '<tr>' +
@@ -107,9 +107,9 @@ function emiToHtmlTable (loan, params) {
     '</table>'
   ]
 
-  for (var i = 0; i < loan.installments.length; i++) {
-    var inst = loan.installments[i]
-    var instHtml =
+  for (let i = 0; i < loan.installments.length; i++) {
+    const inst = loan.installments[i]
+    const instHtml =
           '<tr>' +
             '<td>' + (i + 1) + '</td>' +
             '<td>' + fm(inst.principal) + '</td>' +
@@ -152,8 +152,8 @@ if (typeof module === 'undefined') {
 } else {
   // node or browserfy
   module.exports = {
-    Loan: Loan,
-    emiToHtmlTable: emiToHtmlTable,
-    rnd: rnd
+    Loan,
+    emiToHtmlTable,
+    rnd
   }
 }
