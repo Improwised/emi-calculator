000e126b4898b86681d741353785a4b1b12a584c
Nipul Rathod <nipulrathod5112@gmail.com>
2023-04-08 12:33:34 +0530

    Disallow negative value

--- src/emi.js
+++ src/emi.js
@@ -10,11 +10,11 @@
 function Loan (amount, installmentsNumber, interestRate) {
   /** Checking params */
 // function Loan
-if (!amount || amount <= 0 ||
+  if (!amount || amount <= 0 ||
   !installmentsNumber || installmentsNumber <= 0 ||
   !interestRate || interestRate <= 0) {
- throw new Error(`wrong parameters: ${amount} ${installmentsNumber} ${interestRate}`)
-}
+    throw new Error(`wrong parameters: ${amount} ${installmentsNumber} ${interestRate}`)
+  }
 
   const installments = []
   let interestSum = 0
